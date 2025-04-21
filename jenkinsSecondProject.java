50fbaa7f59aaaba07d560c6dc06b887138ceaac2
Vasanthkumar66 <149463504+Vasanthkumar66@users.noreply.github.com>
2025-04-21 14:01:35 +0530

    Update jenkinsSecondProject.java
    update

--- jenkinsSecondProject.java
+++ jenkinsSecondProject.java
@@ -4,6 +4,7 @@ public static void main(String[] args){
 System.out.println("Second Project");
 System.out.println("How are you");
 System.out.println("How are you");
+ System.out.println("How are you1");
  
 }
 }
