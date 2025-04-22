8854e2a6b77bf8e56a85d8844e897ebe739705b7
Vasanthkumar66 <149463504+Vasanthkumar66@users.noreply.github.com>
2025-04-22 14:03:07 +0530

    hello moto

--- jenkinsSecondProject.java
+++ jenkinsSecondProject.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 System.out.println("Second Project");
 System.out.println("How are you");
 System.out.println("How are you");
- System.out.println("How are you1");
+ System.out.println("How are you1");System.out.println("How are you1");
  
 }
 }
